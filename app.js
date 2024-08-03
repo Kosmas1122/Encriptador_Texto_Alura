1dedece402e276228cbccc74c1a3e56c1b6e25c6
David-Zephyrus <dbekosmas@gmail.com>
2024-08-03 14:28:13 -0500

    Función tratarAcentos()  --> OK

--- app.js
+++ app.js
@@ -17,53 +17,83 @@ const o = "ober";
 const u = "ufat";
 
 function encriptar() {
-  let textoPorEncriptar="";
-  let textoEncriptado ="";
+  let textoPorEncriptar = "";
+  let textoEncriptado = "";
   let cadenaEncriptada = "";
   let k = 0; // Indice cadenaEncriptada.
   let llave = "";
+
+  // Se captura el texto a encriptar en minúsculas:
+  textoPorEncriptar = document.getElementById("texto_Cifrar").value.toLowerCase(); 
+  // Se captura el elemento texto_Descifrar:
+  textoEncriptado = document.getElementById("texto_Descifrar"); 
+
+  if (tratarAcentos(textoPorEncriptar) && true) {
+    alert("Recuerda no utilizar acentos o caracteres especiales!");
+  } 
   
-  textoPorEncriptar = document.getElementById("texto_Cifrar").value.toLowerCase();   // Se captura el texto a encriptar en minúsculas.
-  textoEncriptado = document.getElementById("texto_Descifrar");       // Se captura el elemento texto_Descifrar.
-  
-  //console.log(textoPorEncriptar); // Sonda OK.
+  else 
+  {
+    // Encriptación:
+    for (k = 0; k < textoPorEncriptar.length; k++) {
+      //console.log(textoPorEncriptar[k]); // Sonda OK.
 
-  // Encriptación:
-  for (k = 0; k < textoPorEncriptar.length; k++) {
-    //console.log(textoPorEncriptar[k]); // Sonda OK.
+      switch (textoPorEncriptar[k]) {
+        case "e":
+          llave = e;
+          break;
 
-    switch (textoPorEncriptar[k]) {
-      case "e":
-        llave = e;
-        break;
+        case "i":
+          llave = i;
+          break;
 
-      case "i":
-        llave = i;
-        break;
+        case "a":
+          llave = a;
+          break;
 
-      case "a":
-        llave = a;
-        break;
+        case "o":
+          llave = o;
+          break;
 
-      case "o":
-        llave = o;
-        break;
+        case "u":
+          llave = u;
+          break;
 
-      case "u":
-        llave = u;
-        break;
+        default:
+          llave = textoPorEncriptar[k];
+          break;
+      }
 
-      default:
-        llave = textoPorEncriptar[k];
-        break;
+      cadenaEncriptada = cadenaEncriptada + llave;
     }
 
-    cadenaEncriptada = cadenaEncriptada + llave;
+    textoEncriptado.value = cadenaEncriptada;
   }
 
-  //console.log(cadenaEncriptada);  // Sonda OK.
-  
-  textoEncriptado.value = cadenaEncriptada;
-
   return;
+}
+
+
+
+function tratarAcentos(cadena) {
+  flag = false; // Bandera para indicar presencia/ausencia de acentos.
+
+  // Lista de caracteres acentuados extraida de la Tabla Unicode usando ChatGPT:
+  const caracteresAcentuados = "àáâãäāăąǎǟǡǻȁȃȧǡắằẳẵặảấầẩẫậæèéêëēĕėęěȅȇėẽẹẻếềể \
+    ễệìíîïĩīĭįǐȉȋỉịòóôõöōŏőǒǫǭȍȏǫồốổỗộớờởỡợùúûüũūŭůűųưǔȕȗủứừửữựỳýŷÿȳỹỵỷỷ \
+    ḃćĉċčçďḋḍḏđḟĝğġģĥȟḧḩḫħĵķĺļľḷḹḿṁńņňṅṇṉñṗŕŗřśŝşšṣṡṣťŧṭṯṳṽŵẁẃẅýẋźżžẓẕß";
+
+  for (let i = 0; i < cadena.length; i++) {
+    // Verificar si el carácter actual está en la lista de caracteres acentuados:
+    if (caracteresAcentuados.includes(cadena[i])) {
+      flag = true; // Se encontró un carácter acentuado.
+      break; // Sale del for.
+    } 
+    else 
+    {
+      flag = false;
+    }
+  }
+
+  return flag;
 }
