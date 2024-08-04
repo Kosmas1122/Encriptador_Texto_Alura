8ec7503ac92df1c76799f345104592acd6004ffb
David-Zephyrus <dbekosmas@gmail.com>
2024-08-04 08:46:06 -0500

    Se mejora la función encriptar().

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-/* 
-Llaves de encriptación a utilizar:
+/* Llaves de encriptación a utilizar:
 ----------------------------------
-
 La letra "e" es convertida para "enter"
 La letra "i" es convertida para "imes"
 La letra "a" es convertida para "ai"
 La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat"
+*/
 
+/* Requisitos:
+----------------------------------
+* Debe funcionar solo con letras minúsculas.
+* No deben ser utilizados letras con acentos ni caracteres especiales.
+* Debe ser posible convertir una palabra para la versión encriptada también
+  devolver una palabra encriptada para su versión original.
 */
 
 const e = "enter";
@@ -19,23 +24,42 @@ const u = "ufat";
 
 
 function encriptar() {
-  let textoPorEncriptar = "";
-  let textoEncriptado = "";
-  let cadenaEncriptada = "";
-  let k = 0; // Indice cadenaEncriptada.
-  let llave = "";
+  let flagMinusculas = true;      // Bandera para indicar ausencia de letras mayúsculas.
+  let flagSinAcentos = true;      // Bandera para indicar ausencia de acentos.
+  let flagSinEspeciales = true;   // Bandera para indicar ausencia de caracteres especiales.
 
-  // Se captura el texto a encriptar en minúsculas:
-  textoPorEncriptar = document.getElementById("texto_Cifrar").value.toLowerCase(); 
-  // Se captura el elemento texto_Descifrar:
-  textoEncriptado = document.getElementById("texto_Descifrar"); 
-
-  if (tratarAcentos(textoPorEncriptar) || tratarEspeciales(textoPorEncriptar)) {
-    alert("Recuerda no utilizar acentos o caracteres especiales!");
-  } 
+  let textoPorEncriptar = "";     // Cadena que guarda el texto ingresado por usuario. 
+  let cadenaEncriptada = "";      // Cadena con el texto encriptado.
+  let textoEncriptado = "";       
   
-  else // Encriptación:
-  {
+  let k = 0;                      // Indice cadenaEncriptada.
+  let llave = "";                 // Guarda caracter o una llave.
+
+
+  // Se captura el texto a encriptar:
+  textoPorEncriptar = document.getElementById("texto_Cifrar").value; 
+
+  // Se verifica si hay presencia/ausencia de letras mayúsculas:
+  if(tratarMayusculas(textoPorEncriptar)){
+    alert("¡Recuerda solo usar letras minúsculas!");
+    flagMinusculas = false;
+  }
+
+  // Se verifica si hay presencia/ausencia de acentos:
+  if(tratarAcentos(textoPorEncriptar)){
+    alert("¡Recuerda no utilizar acentos!");
+    flagSinAcentos = false;
+  }
+
+  // Se verifica si hay presencia/ausencia de caracteres especiales:
+  if(tratarEspeciales(textoPorEncriptar)){
+    alert("¡Recuerda no utilizar caracteres especiales!");
+    flagSinEspeciales = false;
+  }
+
+  
+  // Encriptación:
+  if(flagMinusculas && flagSinAcentos && flagSinEspeciales){
     for (k = 0; k < textoPorEncriptar.length; k++) {
     
       switch (textoPorEncriptar[k]) {
@@ -67,6 +91,8 @@ function encriptar() {
       cadenaEncriptada = cadenaEncriptada + llave;
     }
 
+    // Se captura el elemento texto_Descifrar:
+    textoEncriptado = document.getElementById("texto_Descifrar"); 
     textoEncriptado.value = cadenaEncriptada;
   }
 
@@ -75,7 +101,19 @@ function encriptar() {
 
 
 
+function tratarMayusculas(cadena){
+  // La función devuelve true/false si hay presencia/ausencia de letras mayúsculas.
+
+  // La expresión regular busca al menos una letra mayúscula en el texto:
+  const regex = /[A-Z]/;
+  return regex.test(cadena);
+}
+
+
+
 function tratarAcentos(cadena) {
+  // La función devuelve true/false si hay presencia/ausencia de acentos.
+
   flag = false; // Bandera para indicar presencia/ausencia de acentos.
 
   // Lista de caracteres acentuados extraida de la Tabla Unicode usando ChatGPT:
