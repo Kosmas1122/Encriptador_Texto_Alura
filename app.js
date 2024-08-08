d63c7a43eb4597279afd6b4fdc4d21d8be3e2e37
DavidWork <dbekosmas@gmail.com>
2024-08-08 15:44:53 -0500

    Se corrigen caracteres en lista de requerimientos de la app.

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ La letra "u" es convertida para "ufat"
 
 /* Requisitos:
 ----------------------------------
-* Debe funcionar solo con letras minúsculas.
-* No deben ser utilizados letras con acentos ni caracteres especiales.
-* Debe ser posible convertir una palabra para la versión encriptada también
+- Debe funcionar solo con letras minúsculas.
+- No deben ser utilizados letras con acentos ni caracteres especiales.
+- Debe ser posible convertir una palabra para la versión encriptada también
   devolver una palabra encriptada para su versión original.
 */
 
